cc218745a6e1cc6103fbffda69c39e1f94880702
Lucas <lukassmierez@gmail.com>
2024-10-14 19:59:49 -0300

    cambios de contexto para botones

---
+++ script.js
@@ -0,0 +1,26 @@
+const html = document.querySelector('html')
+
+const botonCorto = document.querySelector('.app__card-button--corto')
+
+const botonLargo = document.querySelector('.app__card-button--largo')
+
+const botonEnfoque = document.querySelector('.app__card-button--enfoque')
+
+const banner = document.querySelector('.app__image')
+
+botonCorto.addEventListener('click', () => {
+    cambiarContexto('descanso-corto')
+});
+
+botonEnfoque.addEventListener('click', () => {
+    cambiarContexto('enfoque')
+});
+
+botonLargo.addEventListener('click', () => {
+    cambiarContexto('descanso-largo')
+})
+
+function cambiarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+}
