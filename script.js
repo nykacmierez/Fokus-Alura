0ec8d424626aad1511e1c47d05e0577048ab9c9a
Lucas <lukassmierez@gmail.com>
2024-10-14 22:42:15 -0300

    add destaque en botones

--- script.js
+++ script.js
@@ -4,24 +4,35 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+
 
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto')
+    botonCorto.classList.add('active')
 });
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque')
+    botonEnfoque.classList.add('active')
 });
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo')
+    botonLargo.classList.add('active')
 });
 
 
 function cambiarContexto(contexto) {
+
+    botones.forEach(function(contexto) {
+        contexto.classList.remove('active')
+    });
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
+    
     switch (contexto) {
         case "enfoque":
             titulo.innerHTML = `Optimiza tu productividad,<br>
