6a5d6c10bc4c2a7e7414cfe84d8523b95b7d47f2
Lucas <lukassmierez@gmail.com>
2024-10-14 21:43:25 -0300

    add funciones para el dom

---
+++ script.js
@@ -0,0 +1,25 @@
+const html = document.querySelector('html');
+const botonCorto = document.querySelector('.app__card-button--corto');
+const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+
+
+
+botonCorto.addEventListener('click', () => {
+    cambiarContexto('descanso-corto')
+});
+
+botonEnfoque.addEventListener('click', () => {
+    cambiarContexto('enfoque')
+});
+
+botonLargo.addEventListener('click', () => {
+    cambiarContexto('descanso-largo')
+});
+
+
+function cambiarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+};
