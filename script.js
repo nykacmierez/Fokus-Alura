6aa29f137c25e99239f8b003498264582656fe1c
Lucas <lukassmierez@gmail.com>
2024-10-14 22:05:14 -0300

    add switch para cambios de imagen

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
-
+const titulo = document.querySelector('.app__title');
 
 
 botonCorto.addEventListener('click', () => {
@@ -22,4 +22,23 @@ botonLargo.addEventListener('click', () => {
 function cambiarContexto(contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `./imagenes/${contexto}.png`)
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+            <strong class="app__title-strong">sumérgete en lo q
+            importa.</strong>`
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `
+            Qué tal tomar un respiro?
+            <strong class="app__title-strong">Haz una pausa corta!</strong>`
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `
+            Hora de volver a la superficie
+            <strong class="app__title-strong">Haz una pausa larga!</strong>`
+            break;
+        default:
+            break;
+    };
 };
